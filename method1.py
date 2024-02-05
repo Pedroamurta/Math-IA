effddc68d958e05688cc5399b0a14f9b65256ec0
Pedroamurta <pedroamurta@gmail.com>
2024-02-05 14:23:39 +0000

    Fixing comments

--- method1.py
+++ method1.py
@@ -1,11 +1,13 @@
-ideal_word = 'saaee' #most likely solution not using conditional pobability, not a real word
+#most likely solution not using conditional pobability, not a real word
+ideal_word = 'saaee' 
 
 with open('valid_solutions.txt', 'r') as solutions_text:
     solutions = solutions_text.read().split('\n')
 
 best_words = []
 for word in solutions:
-    similarity_counter = 0 #number of 0 - 5 that counts how much of the word is similar to the ideal word, 5 being the same word
+    #number of 0 - 5 that counts how much of the word is similar to the ideal word, 5 being the same word
+    similarity_counter = 0
     for index in range(len(word)):
         if word[index] == ideal_word[index]:
             similarity_counter += 1
