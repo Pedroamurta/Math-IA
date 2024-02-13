7bd67e075e762da3690531030a6c66693a54556a
Pedroamurta <pedroamurta@gmail.com>
2024-02-13 19:35:02 +0000

    Update calculate_entropy.py

--- calculate_entropy.py
+++ calculate_entropy.py
@@ -9,6 +9,10 @@ for word in word_feedback_probability:
     word_entropy[word] = entropy
 
 best_words = sorted(word_entropy, key = word_entropy.get)[:5]
+worst_words = sorted(word_entropy, key= word_entropy.get, reverse= True)[:5]
 print(best_words)
 for word in best_words:
+    print(word, word_entropy[word])
+print("\n\n")
+for word in worst_words:
     print(word, word_entropy[word])
